ef5904a9903c979247647670edd5650bc881d096
Jack Wonder <babyfacedeveloper@gmail.com>
2021-02-10 22:03:56 +0700

    Add project Block Breaker

---
+++ Block-Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour
+{
+    // config params
+    [SerializeField] AudioClip breakSound = null;
+    [SerializeField] GameObject blockSparklesVFX = null;
+    [SerializeField] Sprite[] hitSprites;
+    //cached references
+    Level level;
+
+    // state variables
+    [SerializeField] int timesHit;
+
+    private void Start()
+    {
+        CountBreakableBlocks();
+    }
+
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        int maxHits = hitSprites.Length + 1;
+        if (timesHit >= maxHits)
+        {
+            DestroyBlock();
+        }
+        else
+        {
+            ShowNextHitSprite();
+        }
+    }
+
+    private void ShowNextHitSprite()
+    {
+        int spriteIndex = timesHit - 1;
+        if(hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.Log("Hit sprite error occurs " + gameObject.name);
+        }
+    }
+
+    private void DestroyBlock()
+    {
+        PlayBlockDestroyedSFX();
+        FindObjectOfType<GameSession>().AddToScore();
+        level.BlockDestroyed();
+        TriggerSparklesVFX();
+    }
+
+    private void PlayBlockDestroyedSFX()
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+        Destroy(sparkles, 1f);
+    }
+}
---
+++ Block-Breaker/Assets/Scripts/Paddle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private float minX = 1f;
+    [SerializeField] private float maxX = 15f;
+    [SerializeField] private float screenWidthInUnits = 16f;
+
+    // cache reference
+    GameSession gameSession;
+    Ball ball;
+
+    void Start()
+    {
+        gameSession = FindObjectOfType<GameSession>();
+        ball = FindObjectOfType<Ball>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(GetXPos(), minX, maxX);
+        transform.position = paddlePos;
+    }
+
+    private float GetXPos()
+    {
+        if(gameSession.IsAutoPlayEnabled())
+        {
+            return ball.transform.position.x;
+        }
+        return Input.mousePosition.x / Screen.width * screenWidthInUnits;
+    }
+}
